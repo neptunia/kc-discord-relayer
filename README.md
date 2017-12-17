71e8464c505432f83b861b35b512e197b0cd4842
neptunia <kevinshen100@gmail.com>
2017-12-16 19:50:12 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+KC to Discord Relayer
+=====================
+
+# Rich Presence Integration for Kancolle!
+
+What it looks like:
+![idle_home](./img/idle.PNG)
+
+## Features
+
+* What map/node you're playing on (if any)
+* Whether you're in PVP or not
+* HQ level
+* Idle/Active indicator
+* Kancolle icon (very important!)
+
+## Usage/Installation
+
+1. install nodejs and dependencies
+2. change chrome to developer mode (chrome://extensions -> developer mode)
+3. Load unpacked extension...
+4. Navigate to this directory and click `extension_part`
+5. Open terminal/cmd.
+6. In the directory of main.js, run `npm run start`
+7. Hope it works :^)
+
+## Contact
+
+Feel free to add an issue or something if something broke/is bugged. Also i have no idea if this will work if you use someone else's discord app. I don't actually know if all the stuff works. Also feel free to put requests and stuff.
---
+++ main.js
@@ -0,0 +1,93 @@
+/* eslint-disable no-console */
+
+const DiscordRPC = require('discord-rpc');
+const fs = require('fs');
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 1234 });
+
+const ClientId = "391369077991538698";
+
+
+var ltext = "Kantai Collection"
+var details = "Home Port - Idle"
+var state = "HQ Level -100"
+var lkey = "kc_logo_512x512"
+var last_time = 0
+var skey = "idle_img"
+var stext = "Idle"
+
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    var x = JSON.parse(message);
+    //console.log(x);
+    if (x["top"] != "none") {
+      details = x["top"];
+    }
+    if (x["bot"] != "none") {
+      state = x["bot"];
+    }
+    
+    console.log('received: %s', message);
+    
+    last_time = Date.now(); //get timestamp
+    skey = "active_img";
+    stext = "Currently Playing!";
+
+  });
+
+});
+
+
+
+
+function getDuration() {
+
+}
+
+DiscordRPC.register(ClientId);
+
+const rpc = new DiscordRPC.Client({
+  transport: 'ipc'
+});
+
+async function setActivity() {
+  if (!rpc)
+    return;
+  var activity = {
+    details: details,
+    state: state,
+    largeImageKey: lkey,
+    largeImageText: ltext,
+    instance: false
+  }
+
+  //check if idle
+  //1000 = second
+  //60000 = minute
+  //300000 = 5 mins
+  var elapsed = Date.now() - last_time;
+  if (elapsed > 300000) {
+    //idle
+    skey = "idle_img";
+    stext = "Idle for " + Math.floor(elapsed/60000) + " minutes";
+  }
+
+  activity.smallImageKey = skey;
+  activity.smallImageText = stext;
+
+  //console.log("gey");
+
+  rpc.setActivity(activity);
+}
+
+rpc.on('ready', () => {
+  setActivity();
+
+  setInterval(() => {
+    setActivity();
+  }, 15e3);
+});
+
+rpc.login(ClientId).catch(console.error);
